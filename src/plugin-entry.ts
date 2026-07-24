f7258b12f88a08d51ae8dd8b32636520ef880c48
陳五學 <bergerchen719@gmail.com>
2026-07-24 11:35:39 +0800

    fix: install rating UI from plugin entry

--- src/plugin-entry.ts
+++ src/plugin-entry.ts
@@ -1,4 +1,11 @@
 import "./search-pagination";
 import "./progress-ui";
-import "./rating-ui";
-export { default } from "./main";
+import AnimeListPlugin from "./main";
+import { installRatingUi } from "./rating-ui";
+
+export default class AnimeListPluginEntry extends AnimeListPlugin {
+  async onload(): Promise<void> {
+    await super.onload();
+    installRatingUi(this);
+  }
+}
